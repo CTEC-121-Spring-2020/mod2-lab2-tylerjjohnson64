e2df58224ca5fb7729630c75819d773a36bc55f5
unknown <t.johnson57@students.clark.edugit config --global user.email t.johnson57@students.clark.edu>
2020-04-29 12:03:05 -0700

    lab 2-2 class demos

---
+++ lab2-2.py
@@ -0,0 +1,57 @@
+"""
+CTEC 121
+Tyler Johnson
+lab 2 mod 2 
+
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+from math import * #imports all from math
+def main():
+    '''
+    myString = "Hello"
+    print(myString)
+    print(type(myString))
+    myInt=33
+    print(myInt)
+    print(type(myInt))
+
+    #print("enter a string: ")
+    astring=input("enter a string: ")
+    print(astring)
+    '''
+
+  
+    '''
+    x, y = eval(input("Enter two numbers separated by a comma: "))
+    print(x, y)
+    print(type(x))
+    print()
+
+    x, y = input("provide two values: ").split()
+    print(x, y)
+    print(type(x))
+    print()
+    '''
+    '''
+    for i in range[10,1,2,3]:
+        print(i)
+
+    for i in range(10):
+        print(i)
+
+
+    for i in range(2,11,2):
+        print(i)
+        '''
+    sum = 0
+    for i in [56, 19, 92, 67]:
+        print(i)
+        sum = sum + i
+    print("---")
+    print(sum)        
+main()    
